07fcae5250a2167dd7453df8b570a664d281d70f
Emmanuelle Jaldorau <e.jaldorau@gmail.com>
2020-07-10 06:03:01 +0200

    Updates

---
+++ precache-manifest.997f88a9135416c0caf97da7d0e190c0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1ff0cadb845d4d0317a1fa9b32794d5",
+    "url": "/zoorit-exo/index.html"
+  },
+  {
+    "revision": "af13f8dad2686e9332af",
+    "url": "/zoorit-exo/static/css/main.b05f2963.chunk.css"
+  },
+  {
+    "revision": "e249088d8d9edaba68b7",
+    "url": "/zoorit-exo/static/js/2.9efa092d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/zoorit-exo/static/js/2.9efa092d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af13f8dad2686e9332af",
+    "url": "/zoorit-exo/static/js/main.77b8b0ea.chunk.js"
+  },
+  {
+    "revision": "1101f4d57b6abdeb7b03",
+    "url": "/zoorit-exo/static/js/runtime-main.60d9d6cf.js"
+  },
+  {
+    "revision": "2af3967f80c376baa5769a27721413c2",
+    "url": "/zoorit-exo/static/media/PetitFormalScript-Regular.2af3967f.woff2"
+  },
+  {
+    "revision": "37017af6d0d02b070e0c18baba3674a5",
+    "url": "/zoorit-exo/static/media/PetitFormalScript-Regular.37017af6.woff"
+  },
+  {
+    "revision": "1f9fea5630d271950cb7763b5060df2b",
+    "url": "/zoorit-exo/static/media/background-palmiers.1f9fea56.jpg"
+  },
+  {
+    "revision": "c920efa82469c6d3f5c85ec878f0e6f4",
+    "url": "/zoorit-exo/static/media/icon-heart.c920efa8.svg"
+  },
+  {
+    "revision": "52bf35757319da43b0dc5e556acaaf03",
+    "url": "/zoorit-exo/static/media/logo-ladress.52bf3575.svg"
+  }
+]);
